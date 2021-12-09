814db2c361b24c8fceb99e0de351012fd200b793
DarkCoder100 <singhsima725@gmail.com>
2021-12-09 15:08:17 +0530

    anything here

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="imgLogo" />
+      <h2>Hello From Vite React</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+// In this video we will host a react project ( basic )
+// The purpose of this video is to actually tech you how to deploy any react app on vercel.
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
